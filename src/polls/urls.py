3ae7a3a46e395e583eaa891133db7301278a6c69
setcain <charlie.bronson00@gmail.com>
2019-10-21 14:22:37 -0500

    adds simple views
    TODO:
    add functionality in views

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index_url'),
+    path('', views.index, name="index_url"),
+    path('<int:question_id>/', views.detail, name="detail_url"),
+    path('<int:question_id>/results/', views.results, name="results_url"),
+    path('<int:question_id>/vote/', views.vote, name="vote_url"),
 ]
--- src/polls/views.py
+++ src/polls/views.py
@@ -6,3 +6,26 @@ def index(request):
     Show simple hello world message in browser
     """
     return HttpResponse("Hello world")
+
+
+def detail(request, question_id):
+    """
+    Detail for one question
+    """
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    """
+    Show result of a question
+    """
+    return HttpResponse(
+        f"You're looking at the results of question {question_id}"
+    )
+
+
+def vote(request, question_id):
+    """
+    Youre vote
+    """
+    return HttpResponse(f"You're voting on question {question_id}")
