8b4f5f027a8924c411dbd654d58b7ed09517f024
setcain <charlie.bronson00@gmail.com>
2019-10-20 21:58:42 -0500

    adds migrations
    TODO:
    try database API

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """
+    Database question and published data
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Fecha de publicación')
+
+
+class Choise(models.Model):
+    """
+    Database choise text and votes count
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
