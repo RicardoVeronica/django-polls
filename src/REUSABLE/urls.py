b34d3dfddee962eccad982bb6e241ab166b3e4c8
setcain <charlie.bronson00@gmail.com>
2019-10-22 12:38:57 -0500

    remove app [polls]

--- src/REUSABLE/urls.py
+++ src/REUSABLE/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.urls import include, path
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
 ]
 
 if settings.DEBUG:
